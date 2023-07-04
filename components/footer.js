6f269d134a9555057203618d462c7e6945509c57
Nahuel <nlcostamagna@gmail.com>
2023-07-04 00:10:29 -0300

    create template

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+import Container from "@/components/container";
+
+export default function Footer(props) {
+  return (
+    <Container className="mt-10 border-t border-gray-100 dark:border-gray-800"></Container>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

68752afed284fe0b76e33381184ee4f1c61cd002
Nahuel <nlcostamagna@gmail.com>
2023-07-14 11:00:52 -0300

    .

--- app/post/1/default.js
+++ app/post/1/default.js
@@ -131,7 +131,8 @@ fmt.Printf("Bitwise XNOR: %d - %.10b\n", ^(a ^ b), ^(a ^ b))
         <meta
           prefix="og: https://www.beeblogit.com#"
           property="og:description"
-          content="Binary Operators in Golang"
+          content="We are going to talk about Binary Operators
+          in Golang"
         />
         <meta
           prefix="og: https://www.beeblogit.com#"
@@ -154,34 +155,8 @@ fmt.Printf("Bitwise XNOR: %d - %.10b\n", ^(a ^ b), ^(a ^ b))
             {post.title}
           </h1>
 
-          <div className="relative z-0 mx-auto hidden aspect-video max-w-screen-lg overflow-hidden lg:rounded-lg">
-            {imageProps && (
-              <Image
-                src={imageProps.src}
-                alt={post.mainImage?.alt || "Thumbnail"}
-                loading="eager"
-                fill
-                sizes="100vw"
-                className="object-cover"
-              />
-            )}
-          </div>
-
           <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
             <div className="flex items-center gap-3">
-              <div className="relative h-10 w-10 flex-shrink-0">
-                {AuthorimageProps && (
-                  <Link href={`/author/${post.author.slug.current}`}>
-                    <Image
-                      src={AuthorimageProps.src}
-                      alt={post?.author?.name}
-                      className="rounded-full object-cover"
-                      fill
-                      sizes="40px"
-                    />
-                  </Link>
-                )}
-              </div>
               <div>
                 <p className="text-gray-800 dark:text-gray-400">
                   <Link href={`/author/${post.author.slug.current}`}>

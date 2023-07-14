344498001adf5150e2d30ec4d16b1bc672da2a45
Nahuel <nlcostamagna@gmail.com>
2023-07-14 10:46:19 -0300

    .

--- app/post/1/default.js
+++ app/post/1/default.js
@@ -136,7 +136,7 @@ fmt.Printf("Bitwise XNOR: %d - %.10b\n", ^(a ^ b), ^(a ^ b))
         <meta
           prefix="og: https://www.beeblogit.com#"
           property="og:image"
-          content="https://www.beeblogit.com/img/post/1/header_900.png"
+          content="https://www.beeblogit.com/img/post/1/header_min.jpg"
         />
         <meta
           prefix="og: https://www.beeblogit.com#"

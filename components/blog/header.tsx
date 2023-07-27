ca785636cb0ef8877dfdac79ec50b475296f5068
Nahuel <nlcostamagna@gmail.com>
2023-07-27 16:23:55 -0300

    fix desc in header

--- components/blog/header.tsx
+++ components/blog/header.tsx
@@ -8,7 +8,7 @@ export default function Header({
   img: string;
 }) {
   const url = "https://www.beeblogit.com";
-  if (desc == "") {
+  if (desc == "" || desc == undefined) {
     desc = title;
   }
 
@@ -18,6 +18,7 @@ export default function Header({
       <link rel="shortcut icon" href="/favicon.png" />
       <meta prefix={`og:${url}#`} property="og:title" content={title} />
       <meta prefix={`og:${url}#`} name="twitter:card" content={title} />
+      <meta name="description" content={desc} />
       <meta prefix={`og:${url}#`} property="og:description" content={desc} />
       <meta prefix={`og:${url}#`} property="og:image" content={url + img} />
       <meta prefix={`og:${url}#`} property="og:url" content={url} />
